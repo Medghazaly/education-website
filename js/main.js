e5f017dd3ebc759e43926491dfe67d0cdd041271
ghazaly <medghazalyegy@gmail.com>
2022-10-26 18:07:14 +0000

    add all pages

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 let navBar = document.querySelector(".header .navbar");
 
-// For Testing
-
-// let links = document.querySelectorAll(".header .link");
-// let linkActive = document.querySelectorAll(".header .links-active");
-
-// links.forEach((link) => {
-//   link.addEventListener("click", () => {
-//     linkActive.forEach((li) => {
-//       li.classList.toggle("active");
-//     });
-//   });
-// });
-
 document.getElementById("menu-btn").onclick = () => {
   navBar.classList.toggle("active");
 };
 
 window.onscroll = () => {
   navBar.classList.remove("active");
+};
+
+let mainVid = document.querySelector(".main-video");
+
+document.querySelectorAll(".course-3 .video video").forEach((vid) => {
+  vid.onclick = () => {
+    let src = vid.getAttribute("src");
+    mainVid.classList.add("active");
+    mainVid.querySelector("video").src = src;
+  };
+});
+
+document.getElementById("close-vid").onclick = () => {
+  mainVid.classList.remove("active");
 };
